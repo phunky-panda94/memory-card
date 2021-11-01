b23f7df0b45b57eaccb27ad1c208f9e86a833b60
phunky-panda94 <victor.nim94@outlook.com>
2021-11-01 11:18:56 +1100

    completed styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import Display from './components/Display';
 
 function App() {
-  return (
+    return (
     <div className="flex flex-col flex-ai-c">
-        <h1 className="heading">Test your memory!</h1>
+        <h1>Test your memory!</h1>
         <div className="container flex flex-row flex-jc-se">
             <h2>Level:</h2>
             <h2>Score:</h2>
         </div>
         <Display />
     </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,14 @@ import Card from "./Card";
 
 function Display(props) {
     return (
-        <div className="container">
+        <div className="container flex flex-row flex-wrap flex-jc-se">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
             <Card />
             <Card />
         </div>
